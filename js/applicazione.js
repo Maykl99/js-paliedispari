0ddb28823ac3eba981d5d7dd60685b5b7a84a227
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-15 14:42:45 +0200

    app js

--- js/applicazione.js
+++ js/applicazione.js
@@ -1,15 +1,18 @@
+/* Palidroma
+Chiedere all'utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
 var nome= prompt('scrivi una parola');
 console.log(palindromo(nome))
 
 
-function palindromo(nome){
+function palindromo(nome,risultato=''){
     var str='';
-    var risultato='';
     for(var i=nome.length-1; i>=0; i--){
         str += nome[i];
         if(str === nome){
             risultato += 'la parola è palindroma';
         }
     }
-    return risultato;
-}
+    return str + ' ' + risultato;
+} 
